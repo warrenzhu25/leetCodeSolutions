101eb6517a3cb291316d5c81f9086a0545c4bb7f
Warren Zhu <zhonzh@microsoft.com>
2021-08-06 12:13:36 -0700

    Write each problems discussion in one file

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ class LeetCodeScrapper {
         const page = await this.browser.newPage();
         await page.setCacheEnabled(false);
         await page.setUserAgent(
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
         );
         return page;
     }
@@ -42,16 +42,14 @@ class LeetCodeScrapper {
         await page.goto(this.url, { waitUntil: "domcontentloaded" });
         await page.waitForSelector(".topic-item-wrap__2FSZ");
 
-        const problemTitle = await page.$eval(
+        const title = await page.$eval(
             ".title__27Kb",
             el => el.textContent
         );
 
-        const title = slugify(problemTitle.toString().trim());
-
         const elements = await page.$$(".topic-item-wrap__2FSZ");
 
-        const input = elements.map(el =>
+        const input = elements.slice(0, 5).map(el =>
             limit(async () => {
                 const solution = await this.getSolutionDetails(el);
                 return solution;
@@ -111,12 +109,11 @@ LeetCodeScrapper.getLeetCodeInstance(url)
 
 async function saveToFile(solutions) {
     const [title, answers] = Object.entries(solutions).pop();
-    const dirName = slugify(title);
-
-    await mkDir(dirName);
+    const questionTitle = slugify(title);
+    writeFile(`${questionTitle}.txt`, `${title}\r\n\r\n`,  {'flag':'a'})
     return Promise.all(
         answers.map(({ title, solution }) =>
-            writeFile(`${dirName}/${slugify(title)}.md`, solution)
+            writeFile(`${questionTitle}.txt`, `${title}\r\n\r\n${solution}\r\n\r\n`,  {'flag':'a'})
         )
     );
 }
